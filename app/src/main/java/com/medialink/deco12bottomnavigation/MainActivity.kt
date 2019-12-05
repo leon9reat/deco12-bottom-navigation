d6fb14dfbc272803d534e7009bb62c2de166c5b9
Bambang <mail.buat.forum@gmail.com>
2019-12-05 14:38:59 +0700

    init

---
+++ app/src/main/java/com/medialink/deco12bottomnavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.medialink.deco12bottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
